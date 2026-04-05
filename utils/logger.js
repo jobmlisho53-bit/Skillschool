16db8a50116a6d888c75de4bff2bb5c0c501cfa3
jobmlisho53-bit <jobmlisho53@gmail.com>
2026-04-05 04:25:11 +0300

    Add Winston logger

---
+++ utils/logger.js
@@ -0,0 +1,19 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: process.env.LOG_LEVEL || 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'logs/combined.log' }),
+        new winston.transports.Console({
+            format: winston.format.simple(),
+            level: 'debug'
+        })
+    ]
+});
+
+module.exports = logger;
